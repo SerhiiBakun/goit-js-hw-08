414483f55a361b8a880b87be0f30b1f8deb2db5a
Serhii Bakun <gizzmo599@gmail.com>
2022-09-26 21:57:10 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import { save, load } from './storage.js';
 
+const vimeoPlayer = document.querySelector('#vimeo-player');
+const player = new Vimeo(vimeoPlayer);
+const TIME_KEY = 'videoplayer-current-time';
+const startTime = load(TIME_KEY);
+
+player.on('timeupdate', throttle(getTime, 1000));
+
+if (startTime) {
+  player
+    .setCurrentTime(startTime.seconds)
+    .then(function (seconds) {})
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+        default:
+          break;
+      }
+    });
+}
+
+function getTime(data) {
+  save(TIME_KEY, data);
+}
