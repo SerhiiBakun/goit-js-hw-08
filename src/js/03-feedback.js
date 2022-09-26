3ce06434c5b14dc2e0f6600647d371aa354ce43b
Serhii Bakun <gizzmo599@gmail.com>
2022-09-26 23:44:29 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
+import { save, load } from './storage.js';
 
+const FORM_KEY = 'feedback-form-state';
+
+const formFeedback = document.querySelector('.feedback-form');
+
+let formData = {};
+completeSavedForm();
+
+formFeedback.addEventListener('input', throttle(onFormInput, 500));
+formFeedback.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  save(FORM_KEY, formData);
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  console.log(load(FORM_KEY));
+  e.currentTarget.reset();
+  localStorage.removeItem(FORM_KEY);
+  formData = {};
+}
+
+function completeSavedForm() {
+  const savedData = load(FORM_KEY);
+  if (savedData) {
+    const keys = Object.keys(savedData);
+    keys.forEach(key => {
+      formData[key] = savedData[key];
+      formFeedback[key].value = savedData[key];
+    });
+  }
+}
