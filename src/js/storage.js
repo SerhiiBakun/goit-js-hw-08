ac3a6f101a51dbbfa001b03009d3acfba07304c8
Serhii Bakun <gizzmo599@gmail.com>
2022-11-21 20:59:47 +0200

    Update storage.js

--- src/js/storage.js
+++ src/js/storage.js
@@ -16,4 +16,12 @@ const load = key => {
   }
 };
 
-export { save, load };
+const remove = key => {
+  try {
+    localStorage.removeItem(key);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+export { save, load, remove };
